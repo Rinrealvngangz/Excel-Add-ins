b4b61922de8696f1226926ddb0dc2378ae94b6b2
rinrealvngangz <tuannmctk42@gmail.com>
2022-03-08 23:27:28 +0700

    add feature json sub

--- JsonToExcel/src/taskpane/taskpane.js
+++ JsonToExcel/src/taskpane/taskpane.js
@@ -39,42 +39,81 @@ export async function run() {
     function fillJsonObjectToData(dataJson){
       let props =[];
       let resultData =[];
-
-      dataJson.forEach(el =>{
-        let dataProps =[];
+      let dictionary = {};
+      dataJson.forEach((el,index) =>{
+       
+        //let dataProps =[];
         let nodeRoots =[];
-       mapKeyToValue(props,dataProps,el,nodeRoots);
-        resultData.push(dataProps);
+        const valueIndex = {
+           el: el,
+           index: index
+        }
+           // dictionary:{"name":["rin","tuan"] , "age":[22,20]}
+        mapKeyToValue(dictionary,dataJson.length,valueIndex,nodeRoots);
+      //  resultData.push(dataProps);
       });
+      dictionaryToStructureExcel(resultData,dictionary,props);
       resultData.unshift(props);
           return resultData;
     }
       //props[]:key, dataProps[]:values, el:{} , nameKey:format name key
-    function mapKeyToValue(props,dataProps,el,nodeRoots){
-      for( let key in el){
-        if(el.hasOwnProperty(key)){
-              if(el[key] !== null && el[key].constructor === ({}).constructor){
+    function mapKeyToValue(dictionary,length,valueIndex,nodeRoots){
+      for( let key in valueIndex.el){
+        const dataProps = createTemp(length);
+        if(valueIndex.el.hasOwnProperty(key)){
+              if(valueIndex.el[key] !== null && valueIndex.el[key].constructor === ({}).constructor){
                 if(nodeRoots.length >0){
                    nodeRoots[0] = nodeRoots[0] +"/"+ key; // ["contact/address"]
                 }else{
                  nodeRoots.push(key);// ["contact"];
                 }
-                 mapKeyToValue(props,dataProps,el[key],nodeRoots);
+                let valueSub = {
+                  el:valueIndex.el[key],
+                  index: valueIndex.index
+                }
+                 mapKeyToValue(dictionary,dataProps,valueSub,nodeRoots);
                  nodeRoots.shift();
              }
              else{
-              if(props.indexOf(key) === -1){
-                if(nodeRoots.length >0){
-                  if(props.indexOf(nodeRoots[0]+ "/" + key)=== -1){
-                    props.push(nodeRoots[0] + "/" + key);//contact/sdt
-                  }
-                }else{
-                  props.push(key);
-                }
-            }        
-              dataProps.push(el[key]);
+                   if(nodeRoots.length >0){
+                     if(!dictionary.hasOwnProperty(nodeRoots[0]+ "/" + key)){
+                      dictionary[nodeRoots[0]+ "/" + key] = dataProps;
+                    }
+                    dictionary[nodeRoots[0] + "/" + key][valueIndex.index] = valueIndex.el[key];
+                   }
+                   else{
+                     if(!dictionary.hasOwnProperty(key)){
+                      dictionary[key] = dataProps;//{"name":[null,null,...n]}
+                    
+                     }
+                     dictionary[key][valueIndex.index] = valueIndex.el[key];// replace null to value of key
+                   }
+              
              } 
         }
       }
+    }
+
+    function dictionaryToStructureExcel(data,dictionary,props){
+      for( let key in dictionary){//dictionary:{"name":["rin,"trien"] , "age":[22,20]}
+          if(props.indexOf(key) === -1){
+            props.push(key);
+          }
+      }
+      for(let i =0 ; i<props.length ;i++){
+        const values = [];
+        for(let j =0 ;j<props.length; j++){
+          values.push(dictionary[props[j]][i]); // ["rin",22]
+        }
+        data.push(values);
+      }
+    }
+
+    function createTemp(length){
+      let temps = [];
+        for(var i = 0; i < length; i++) {
+            temps.push(null);
+        }
+        return temps;
     }
 
