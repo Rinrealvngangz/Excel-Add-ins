b8a6a3fd12a24f7799418b5d5f8a32b74657d91a
rinrealvngangz <tuannmctk42@gmail.com>
2022-03-16 20:44:26 +0700

    design UI

--- Ex-Tool/src/taskpane/taskpane.js
+++ Ex-Tool/src/taskpane/taskpane.js
@@ -5,50 +5,11 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = test;
+    document.getElementById("run").onclick = run;
     document.getElementById("clear").onclick = clear;
   }
 });
 
-async function test(){
-  await Excel.run(async (context) => {
-    let sheet = context.workbook.worksheets.getActiveWorksheet();
-
-    // Create the headers and format them to stand out.
-    let headers = [
-      ["Product", "Quantity", "Unit Price", "Totals"]
-    ];
-    let headerRange = sheet.getRange("B2:E2");
-    headerRange.values = headers;
-    headerRange.format.fill.color = "#4472C4";
-    headerRange.format.font.color = "white";
-
-    // Create the product data rows.
-    let productData = [
-      ["Almonds", 6, 7.5],
-      ["Coffee", 20, 34.5],
-      ["Chocolate", 10, 9.56],
-    ];
-    let dataRange = sheet.getRange("B3:D5");
-    dataRange.values = productData;
-
-    // Create the formulas to total the amounts sold.
-    let totalFormulas = [
-      ["=C3 * D3"],
-      ["=C4 * D4"],
-      ["=C5 * D5"],
-      ["=SUM(E3:E5)"]
-    ];
-    let totalRange = sheet.getRange("E3:E6");
-    totalRange.formulas = totalFormulas;
-    totalRange.format.font.bold = true;
-
-    // Display the totals as US dollar amounts.
-    totalRange.numberFormat = [["$0.00"]];
-
-    await context.sync();
-});
-}
 
 export async function run() {
       let textJson = document.getElementById("data-json").value;
